50f766d476f4b7fa6b5b83beed4b19b9a2a8e63f
J.D. Zamfirescu <zamfire@gmail.com>
2015-08-20 08:18:57 -0700

    cleanup

--- run-python.js
+++ run-python.js
@@ -1,20 +1,9 @@
-// output functions are configurable.  This one just appends some text
-// to a pre element.
-function outf(text) {
-  console.log("OUT", text);
-    // var mypre = document.getElementById("output");
-    // mypre.innerHTML = mypre.innerHTML + text;
-} 
 function builtinRead(x) {
     if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
             throw "File not found: '" + x + "'";
     return Sk.builtinFiles["files"][x];
 }
-// Here's everything you need to run a python program in skulpt
-// grab the code from your textarea
-// get a reference to your pre element for output
-// configure the output function
-// call Sk.importMainWithBody()
+
 function wrapFunction(f) {
   return new Sk.builtin.func(function() {
     return Sk.ffi.remapToPy(f.apply(f, Array.prototype.map.call(arguments, function(arg) { return Sk.ffi.remapToJs(arg); })));
@@ -23,7 +12,7 @@ function wrapFunction(f) {
 
 function getHeuristicFromPython(code, output_f, globals) { 
    var mypre = document.getElementById("output"); 
-   // mypre.innerHTML = ''; 
+
    Sk.pre = "output";
    Sk.configure({output:output_f, read:builtinRead}); 
    
@@ -33,7 +22,7 @@ function getHeuristicFromPython(code, output_f, globals) {
        mod.$d[k] = typeof(globals[k]) == 'function' ? wrapFunction(globals[k]) : Sk.ffi.remapToPy(globals[k]);
      }
    }
-   console.log(mod);
+
    return function(next, from, to, path) {
      return Sk.ffi.remapToJs(Sk.misceval.call(mod.$d.heuristic, null, null, null, 
        Sk.ffi.remapToPy(next),
@@ -42,15 +31,4 @@ function getHeuristicFromPython(code, output_f, globals) {
        Sk.ffi.remapToPy(path) // remapToPy is recursive
      ));
    }
-   // var myPromise = Sk.misceval.asyncToPromise(function() {
-   //     return
-   // });
-   // myPromise.then(function(mod) {
-   //     console.log('success');
-   //     console.log(mod);
-   //     cb(mod.$d.heuristic);
-   // },
-   // function(err) {
-   //     console.log(err.toString());
-   // });
 } 
